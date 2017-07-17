5c910951c600d7ec5971a3e3f6f6306d469f323b
Radka Janek <radka.janek@redhat.com>
2017-07-17 21:07:41 +0200

    +Simple WebApi

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace dotnettalk
 {
@@ -12,37 +15,78 @@ namespace dotnettalk
 
         static void Main(string[] args)
         {
-            CliAsync().Wait();
+            CliAsync();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<WebStartup>()
+                .UseUrls("http://localhost:5000")
+                .Build();
+            host.Run();
+        }
+
+        public class WebStartup
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+                app.Run(async context =>
+                {
+                    string serviceName = "httpd.service";
+
+                    switch( context.Request.Path )
+                    {
+                        case "/status":
+                            await context.Response.WriteAsync(await Systemctl.GetServiceStatus(serviceName));
+                            break;
+                        case "/restart":
+                            await context.Response.WriteAsync("Restarting " + serviceName);
+                            await Systemctl.RestartService(serviceName);
+                            break;
+                        default:
+                            await context.Response.WriteAsync(HelpText);
+                            break;
+
+                    }
+                });
+            }
         }
 
         static async Task CliAsync()
         {
-            string input = "";
-            string serviceName = "httpd.service";
-            string helpText = HelpText + "\n  quit    - exit the cli interface";
-
-            Console.WriteLine(helpText);
-
-            while( (input = Console.ReadLine()) != "quit" )
+            try
             {
-                Console.WriteLine("");
-                switch(input)
+                await Task.Delay(300);
+                string input = "";
+                string serviceName = "httpd.service";
+                string helpText = HelpText + "\n  quit    - exit the cli interface";
+
+                Console.WriteLine(helpText);
+
+                while( (input = Console.ReadLine()) != "quit" )
                 {
-                    case "help":
-                        Console.WriteLine(helpText);
-                        break;
-                    case "status":
-                        Console.WriteLine(await Systemctl.GetServiceStatus(serviceName));
-                        break;
-                    case "restart":
-                        await Systemctl.RestartService(serviceName);
-                        Console.WriteLine("Restarting " + serviceName);
-                        break;
-                    default:
-                        Console.WriteLine("Invalid command. Use `help` for help.");
-                        break;
+                    Console.WriteLine("");
+                    switch(input)
+                    {
+                        case "help":
+                            Console.WriteLine(helpText);
+                            break;
+                        case "status":
+                            Console.WriteLine(await Systemctl.GetServiceStatus(serviceName));
+                            break;
+                        case "restart":
+                            await Systemctl.RestartService(serviceName);
+                            Console.WriteLine("Restarting " + serviceName);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid command. Use `help` for help.");
+                            break;
+                    }
+                    Console.WriteLine("");
                 }
-                Console.WriteLine("");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
