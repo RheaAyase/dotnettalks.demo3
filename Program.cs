16b4c6f030993a82ed02669fd28ee2b28fc3484b
Radka Janek <radka.janek@redhat.com>
2017-07-17 16:28:24 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace dotnettalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hai!");
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            Console.WriteLine("httpd status: ");
+            Console.WriteLine(await Systemctl.GetServiceStatus("httpd.service"));
+            await Systemctl.RestartService("httpd.service");
+            Console.WriteLine(await Systemctl.GetServiceStatus("httpd.service"));
+
+            await Task.Run(() => {
+                Console.WriteLine("Press any key to close the application.");
+                Console.Read();
+            });
+        }
+    }
+}
---
+++ Systemctl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Tmds.DBus;
+using Systemd1.DBus;
+
+namespace dotnettalk
+{
+	public class Systemctl
+	{
+		public static async Task<string> GetServiceStatus(string serviceName)
+		{
+			string statusString = "";
+			using( var connection = new Connection(Address.System) )
+			{
+				await connection.ConnectAsync();
+				IManager systemd = connection.CreateProxy<IManager>("org.freedesktop.systemd1",
+					"/org/freedesktop/systemd1");
+
+				Unit[] units = await systemd.ListUnitsAsync();
+				Unit unit = units.FirstOrDefault(u => u.Name == serviceName);
+
+				statusString = string.Format("Service Name: `{0}`\n" +
+				                             "Status: `{1}` - `{2}`", unit.Name, unit.ActiveState, unit.SubState);
+			}
+
+			return statusString;
+		}
+
+		public static async Task<bool> RestartService(string serviceName)
+		{
+			using( Connection connection = new Connection(Address.System) )
+			{
+				await connection.ConnectAsync();
+				IManager systemd = connection.CreateProxy<IManager>("org.freedesktop.systemd1",
+					"/org/freedesktop/systemd1");
+
+				await systemd.RestartUnitAsync(serviceName, "fail");
+			}
+			return true; //TODO: actually read returned job...
+		}
+	}
+}
